0a1c3008982d5c334cde57342f7d333a182229af
wkswaileh <wkswaileh@gmail.com>
2024-05-30 15:20:02 +0000

    Initial commit

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { CheckboxModule } from 'primeng/checkbox';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule, PasswordModule, FormsModule, CheckboxModule, RouterLink
+  ],
+  templateUrl: './login.component.html',
+  styles: [`
+  :host ::ng-deep .pi-eye,
+  :host ::ng-deep .pi-eye-slash {
+      transform:scale(1.6);
+      margin-right: 1rem;
+      color: var(--primary-color) !important;
+  }
+`],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginComponent {
+  valCheck: string[] = ['remember'];
+
+  password!: string;
+}
